738ff920d676e49e77499a6948313982b26e2170
BAYECHOU Abdelhadi <abdelhadi.bayechou@buawei.com>
2022-05-09 01:24:10 +0200

    implement notfound

--- src/views/notFound/notFount.tsx
+++ src/views/notFound/notFount.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function NotFond() {
   return (
-    <div>404</div>
+    <div className="d-flex justify-content-center align-items-center mt-5"> 
+      <img  src={process.env.PUBLIC_URL + "/img/404.png"} alt="loading..." />
+    </div>
   )
 }
